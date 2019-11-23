7ddf2a0d684295f146b78b22c218ecf4d4c92a0d
Lxt1213 <1766499892@qq.com>
2019-11-23 21:35:28 +0800

    1

--- src/game/GameInterface.java
+++ src/game/GameInterface.java
@@ -4,5 +4,6 @@ public class GameInterface {
     public static void main(String[] args){
         System.out.println("1");
         System.out.println("2");
+        System.out.print("13");
     }
 }
