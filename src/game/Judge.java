177c7fc7c873e065663e239b3723988d89f1c21e
Lxt1213 <1766499892@qq.com>
2019-11-29 22:25:38 +0800

    0.0

--- src/game/Judge.java
+++ src/game/Judge.java
@@ -8,7 +8,7 @@ public class Judge {
             for (int offsetY=-1;offsetY<=1;offsetY++){
                 if(offsetX==0&&offsetY==0)continue;
                 int indexX=offsetX+x,indexY=offsetY+y;
-                if(isInside(indexX,indexY)&&chessboard[indexX][indexY].isSame(color)==false){
+                if(isInside(indexX,indexY)&&chessboard[indexX][indexY].isSame(color)==false&&chessboard[indexX][indexY]!=Color.NULL){
                     for (int i=indexX+offsetX,j=indexY+offsetY;isInside(i,j);i+=offsetX,j+=offsetY){
                         if(chessboard[i][j]==Color.NULL){
                             break;
@@ -21,21 +21,20 @@ public class Judge {
         }
         return false;
     }
-    //TODO:判断输赢
-    public static boolean judgeIsOver(Color[][] chessboard){
-        if(isStalemate(Color.BLACK,chessboard) && isStalemate(Color.WHITE,chessboard)) {
-            return true;
-        }
-        else{
-            return false;
-        }
+    //TODO:判断是否结束
+    public static boolean judgeIsOver(){
+
+
+        return true;
     }
     //TODO:判断是否有子可落
     public static boolean isStalemate(Color color,Color[][] chessboard){
         for (int i=0;i<8;i++){
             for (int j=0;j<8;j++){
                 if(chessboard[i][j]==Color.NULL){
-                    return judgeDrap(i,j,color,chessboard);
+                    if(judgeDrap(i,j,color,chessboard)){
+                        return true;
+                    }
                 }
             }
         }
