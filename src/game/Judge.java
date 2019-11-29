0f6218fce75fcbe55d9e3103ae4d33978fb7e528
Lxt1213 <1766499892@qq.com>
2019-11-29 21:48:31 +0800

    0.0

--- src/game/Judge.java
+++ src/game/Judge.java
@@ -22,10 +22,13 @@ public class Judge {
         return false;
     }
     //TODO:判断输赢
-    public static boolean judgeIsOver(){
-
-
-        return true;
+    public static boolean judgeIsOver(Color[][] chessboard){
+        if(isStalemate(Color.BLACK,chessboard) && isStalemate(Color.WHITE,chessboard)) {
+            return true;
+        }
+        else{
+            return false;
+        }
     }
     //TODO:判断是否有子可落
     public static boolean isStalemate(Color color,Color[][] chessboard){
